8295248c9ead25c08abba77ab34873bc7b2afb03
league <gmjangwoo0827@gmail.com>
2024-05-25 18:00:34 -0700

    almost finished robot race and I finished the level 1 test today.

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,15 +1,31 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
+	
+	
 
 	//2. create an array of 5 robots.
-
+	Robot[] robots = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+    for(int i = 0; i<robots.length;i++) {
+    	robots[i] = new Robot();
+    	robots[i].setX(100);
+    	robots[i].setY(100);
+    	robots[i].setPenWidth(100);
+    }
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+    ;
 	//5. use another for loop to iterate through the array and make each robot move 
+    	Random rand = new Random();
+    	for(int i = 0; robots[i].move(); i++) {
+    		i = rand.nextInt(49);
+    	}
 	//   a random amount less than 50.
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
@@ -19,4 +35,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
